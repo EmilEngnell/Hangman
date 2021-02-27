671dbf73e7e773744b937a953a967df887241685
Emil <Emil@LAPTOP-PV56MM27>
2021-02-27 17:35:54 +0100

    input

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Hangman
@@ -7,6 +9,7 @@ namespace Hangman
     {
         static void Main(string[] args)
         {
+         
             Console.WriteLine("You wanna play a game?\nLet's do a round of Hangman!");
             Console.WriteLine(" \nYou have been found at a murderscene\nAnd the polise suspects you of being the murderer\nHow scary");
             Console.WriteLine("Your only chanse of escape execution through hanging is to catch the real killer\n A killer that you saw running away from the scene");
@@ -29,7 +32,7 @@ namespace Hangman
             listwords[14] = "mengele";
             listwords[15] = "cuomo";
             listwords[16] = "kingping";
-            listwords[17] = "ivan";
+            listwords[17] = "macbeth";
             listwords[18] = "brutus";
             listwords[19] = "stalin";
             listwords[20] = "ciccero";
@@ -41,19 +44,70 @@ namespace Hangman
             var idx = randGen.Next(0, 24);
             string mysteryWord = listwords[idx];
             char[] guess = new char[mysteryWord.Length];
-            Console.WriteLine("Pick a letter for your clue");
 
+            StringBuilder displayToPlayer = new StringBuilder(mysteryWord.Length);
             for (int p = 0; p < mysteryWord.Length; p++)
                 guess[p] = '_';
 
-            while (true)
+            List<char> correctLetter = new List<char>();
+            List<char> incorrectLetter = new List<char>();
+
+            int lives = 10;
+            bool won = false;
+            int lettersRevealed = 0;
+
+            string input;
+            char playerGuess;
+
+            while (!won && lives > 0);
             {
-                char playerGuess = char.Parse(Console.ReadLine());
-                for (int j = 0; j < mysteryWord.Length; j++)
+                Console.WriteLine("Pick a letter for your clue");
+
+                input = Console.ReadLine().ToUpper();
+                playerGuess = input[0];
+
+                if (correctLetter.Contains(playerGuess))
                 {
-                    if (playerGuess == mysteryWord[j])
-                        guess[j] = playerGuess;
+                    Console.WriteLine("Yeah, I heard you last time you said '{0}', you need some other clues as well.", playerGuess);
+                    continue;
                 }
+                else if (incorrectLetter.Contains(playerGuess))
+                {
+                    Console.WriteLine("Look, you have already tried '{0}', and it's still wrong.", playerGuess);
+                    continue;
+                }
+
+                if (mysteryWord.Contains(playerGuess))
+                {
+                    correctLetter.Add(playerGuess);
+
+                    //char playerGuess = char.Parse(Console.ReadLine());
+                    for (int j = 0; j < mysteryWord.Length; j++)
+                    {
+                        if (playerGuess == mysteryWord[j])
+                        {
+                            guess[j] = playerGuess;
+                            lettersRevealed++;
+
+                        }
+
+                    }
+
+                    if (lettersRevealed == mysteryWord.Length)
+                        won = true;
+                }
+                else
+                {
+                    incorrectLetter.Add(playerGuess);
+
+                    Console.WriteLine("Oops! Looks like '{0}' has no connection to the real culprit!");
+                    lives--;
+                }
+
+                if (won)
+                    Console.WriteLine("Congratulations! You gathered enought clues to catch the real killer!\n You have managed to survive the Hangman\n Unlike the real killer.");
+                else
+                    Console.WriteLine("How sad, the killer got away...\n And you got Hanged in his place");
                 Console.WriteLine(guess);
             }
         }
